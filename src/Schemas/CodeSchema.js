958d609b3b3d9a379f025f816744bad706b42632
XegSo <121321053+XegSo@users.noreply.github.com>
2023-09-14 14:38:20 -0500

    Delete src/Schemas/CodeSchema.js

--- src/Schemas/CodeSchema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose')
-const vcode = new Schema({
-  UserID: String,
-  VerificationCode: String,
-  OsuName: String,
-  Verified: String,
-})
-
-module.exports = model("vcode", vcode)
