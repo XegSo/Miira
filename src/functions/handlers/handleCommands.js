528e2b4f48ca0f863254b9a2cc961bbaf7f09e70
XegSo <xeresonance@gmail.com>
2023-09-17 21:40:36 -0500

    hard reset fix

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const { REST, Routes } = require('discord.js');
+const rest = new REST().setToken(process.env.TOKEN);
+const clientId = '959278677789663302';
+const guildId = '630281137998004224';
 
 module.exports = (client) => {
-    client.handleCommands = async() => {
+    client.handleCommands = async () => {
         const { commands, commandArray } = client;
 
         fs.readdirSync(path.join(__dirname, "../../commands/tools")).filter((file) => {
@@ -15,11 +19,16 @@ module.exports = (client) => {
         })
     }
 
-    // Si SlashCommands se duplican entonces hacer hardreset.
-    client.hardReset = async()  => {
-        //const guild = await client.guilds.fetch('630281137998004224');
-        const { commands } = client;
-        commands.set([]);
-        //guild.commands.set([]);
+    // If the fuckers dupe then hardreset them.
+    client.hardReset = async () => {
+        // for guild-based commands
+        rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+            .then(() => console.log('Successfully deleted all guild commands.'))
+            .catch(console.error);
+
+        // for global commands
+        rest.put(Routes.applicationCommands(clientId), { body: [] })
+            .then(() => console.log('Successfully deleted all application commands.'))
+            .catch(console.error);
     }
 };
