1277f7252e3fbf08fa6885173272affd61d8acd0
XegSo <xeresonance@gmail.com>
2023-09-17 21:43:57 -0500

    handleCommands refactor

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -17,6 +17,23 @@ module.exports = (client) => {
                 console.log(`Command: ${command.data.name} has been loaded.`);
             }
         })
+
+        (async () => {
+            try {
+                console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        
+                // The put method is used to fully refresh all commands in the guild with the current set
+                const data = await rest.put(
+                    Routes.applicationGuildCommands(clientId, guildId),
+                    { body: commands },
+                );
+        
+                console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+            } catch (error) {
+                // And of course, make sure you catch and log any errors!
+                console.error(error);
+            }
+        })();
     }
 
     // If the fuckers dupe then hardreset them.
