8916ba3591e609c933d1ff8bd341abbef2850764
XegSo <xeresonance@gmail.com>
2023-09-17 22:32:25 -0500

    timeout commands

---
+++ src/commands/moderation/timeout.js
@@ -0,0 +1,67 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField, Embed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('timeout')
+        .setDescription('Timeout a user (Mod Only).')
+        .addUserOption(option => 
+            option
+                .setName('user')
+                .setDescription('User to timeout.')
+                .setRequired(true)
+        )
+        .addIntegerOption(option => 
+            option
+                .setName('minutes')
+                .setDescription('Amount of minutes to timeout.')
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option
+                .setName('reason')
+                .setDescription('Reason of timeout.')
+        ),
+    async execute(int, client) {
+        await int.deferReply({ ephemeral: true });
+        const timeUser = int.options.getUser('user');
+        const timeMember = await int.guild.members.fetch(timeUser.id);
+        const duration = int.options.getInteger('minutes')*60;
+
+        if (!int.member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
+            int.editReply({ content: 'You don\'t have the permissions for this.', ephemeral: true });
+            return;
+        }
+        if (!timeMember.kickable) {
+            int.editReply({ content: 'I don\'t have the permissions to timeout this user.', ephemeral: true });
+            return;
+        }
+        if (int.member.id === timeMember.id || timeMember.permissions.had(PermissionsBitField.Flags.Administrator)) {
+            int.editReply({ content: 'the fuck are you doing lol.', ephemeral: true });
+            return;
+        }
+        if (!duration || !timeMember) {
+            int.editReply({ content: 'Please provide valid fields.', ephemeral: true });
+            return;
+        }
+
+        let reason = int.options.getString('reason') || 'No reason given.';
+
+        await timeMember.timeout(duration * 1000, reason);
+
+        const embed = new EmbedBuilder()
+            .setImage('https://puu.sh/JPffc/3c792e61c9.png')
+            .setColor('#f26e6a')
+            .setDescription(`:white_check_mark: ${timeUser.tag} has been timed out for ${duration / 60 } minute(s) | Reason: ${reason}`)
+
+        const dmEmbed = new EmbedBuilder()
+            .setImage('https://puu.sh/JPffc/3c792e61c9.png')
+            .setColor('#f26e6a')
+            .setDescription(`:white_check_mark: You have been timed out in ${int.guild.name}. You can check the status in the server. | Reason: ${reason}`)  
+            
+        await timeMember.send({ embeds: [dmEmbed] }).catch(err => {
+            return;
+        });
+        
+        await int.reply({ embeds: [embed] });
+    }    
+}
---
+++ src/commands/moderation/untimeout.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField, Embed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('untimeout')
+        .setDescription('Untimeout a user (Mod Only).')
+        .addUserOption(option => 
+            option
+                .setName('user')
+                .setDescription('User to untimeout.')
+                .setRequired(true)
+        )
+        .addStringOption(option => 
+            option
+                .setName('reason')
+                .setDescription('Reason of untimeout.')
+        ),
+    async execute(int, client) {
+        await int.deferReply({ ephemeral: true });
+        const timeUser = int.options.getUser('user');
+        const timeMember = await int.guild.members.fetch(timeUser.id);
+
+        if (!int.member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
+            int.editReply({ content: 'You don\'t have the permissions for this.', ephemeral: true });
+            return;
+        }
+        if (!timeMember.kickable) {
+            int.editReply({ content: 'I don\'t have the permissions to untimeout this user.', ephemeral: true });
+            return;
+        }
+        if (int.member.id === timeMember.id || timeMember.permissions.had(PermissionsBitField.Flags.Administrator)) {
+            int.editReply({ content: 'the fuck are you doing lol.', ephemeral: true });
+            return;
+        }
+        if (!timeMember) {
+            int.editReply({ content: 'Please provide valid fields.', ephemeral: true });
+            return;
+        }
+
+        let reason = int.options.getString('reason') || 'No reason given.';
+
+        await timeMember.timeout(null, reason);
+
+        const embed = new EmbedBuilder()
+            .setImage('https://puu.sh/JPffc/3c792e61c9.png')
+            .setColor('#f26e6a')
+            .setDescription(`:white_check_mark: ${timeUser.tag}'s timeout has been removed. | Reason: ${reason}`)
+
+        const dmEmbed = new EmbedBuilder()
+            .setImage('https://puu.sh/JPffc/3c792e61c9.png')
+            .setColor('#f26e6a')
+            .setDescription(`:white_check_mark: You have been untimed out in ${int.guild.name}. | Reason: ${reason}`)  
+            
+        await timeMember.send({ embeds: [dmEmbed] }).catch(err => {
+            return;
+        });
+        
+        await int.reply({ embeds: [embed] });
+    }    
+}
