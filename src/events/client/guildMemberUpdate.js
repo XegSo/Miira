282a5f5ade674434de6ee764e0ac04e48562840a
XegSo <xeresonance@gmail.com>
2023-09-20 22:21:31 -0500

    hotfix

--- src/events/client/guildMemberUpdate.js
+++ src/events/client/guildMemberUpdate.js
@@ -7,9 +7,9 @@ module.exports = {
         let memberUpdated = (await member.guild.members.fetch({ user: member.user.id, force: true }));
         const roles = memberUpdated.roles.cache.map(role => role.name);
         badges = localFunctions.updateBadges(roles);
-        let userInventory = await localFunctions.getInventory(userId, collection) || [];
-        let onUse = await localFunctions.getOnUse(userId, collection);
         const { collection, client: mongoClient } = await connectToMongoDB("OzenCollection");
+        let userInventory = await localFunctions.getInventory(member.user.id,  collection) || [];
+        let onUse = await localFunctions.getOnUse(member.user.id, collection);
         try {
             await localFunctions.setBadges(member.user.id, badges, collection);
             console.log(`Badges for user ${member.user.tag} have been updated`); 
