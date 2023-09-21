51f3738dca8cf2ded411ac6c84e376448c479bde
XegSo <xeresonance@gmail.com>
2023-09-20 22:20:01 -0500

    cosmetic update on memberupdate

--- src/events/client/guildMemberUpdate.js
+++ src/events/client/guildMemberUpdate.js
@@ -7,11 +7,14 @@ module.exports = {
         let memberUpdated = (await member.guild.members.fetch({ user: member.user.id, force: true }));
         const roles = memberUpdated.roles.cache.map(role => role.name);
         badges = localFunctions.updateBadges(roles);
-        console.log(badges)
+        let userInventory = await localFunctions.getInventory(userId, collection) || [];
+        let onUse = await localFunctions.getOnUse(userId, collection);
         const { collection, client: mongoClient } = await connectToMongoDB("OzenCollection");
         try {
             await localFunctions.setBadges(member.user.id, badges, collection);
-            console.log(`Badges for user ${member.user.tag} have been updated`);
+            console.log(`Badges for user ${member.user.tag} have been updated`); 
+            localFunctions.updateNonPurchaseableCosmetics(member.user.id, collection, roles, userInventory, onUse) //Updates cosmetics
+            console.log(`Cosmetics for user ${member.user.tag} have been updated`); 
         } finally {
             mongoClient.close();
         }
