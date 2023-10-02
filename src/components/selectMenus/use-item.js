495dea39fd8de978c4d69286a153b7bda2b082f5
XegSo <xeresonance@gmail.com>
2023-10-02 13:02:05 -0500

    hotfix

--- src/components/selectMenus/use-item.js
+++ src/components/selectMenus/use-item.js
@@ -8,6 +8,7 @@ module.exports = {
     name: 'use-item'
   },
   async execute(int, client) {
+    await int.deferReply({ ephemeral: true });
     const userId = int.user.id;
     const guild = int.guild;
     const selectedItem = int.values[0];
@@ -30,22 +31,22 @@ module.exports = {
           // Set the boost end time in the database
           await localFunctions.setBoostEndTime(userId, newBoostEndTime, collection);
 
-          int.reply(`<@${userId}> has activated a 2X Tokens Earned Boost for 72 hours!`);
+          int.editReply(`<@${userId}> has activated a 2X Tokens Earned Boost for 72 hours!`);
         } else {
-          int.reply({ content: `You already have an active Tokens Earned Boost.`, ephemeral: true });
+          int.editReply({ content: `You already have an active Tokens Earned Boost.`, ephemeral: true });
         }
       } else if (selectedItem === 'Permanent X2 Boost') {
         const PermaBoost = await localFunctions.getPermaBoost(userId, collection);
 
         if (!PermaBoost) {
           await localFunctions.setPermaBoost(userId, true, collection);
-          int.reply(`<@${userId}> has activated a Permanent 2X Tokens Earned Boost!`);
+          int.editReply(`<@${userId}> has activated a Permanent 2X Tokens Earned Boost!`);
         } else {
-          int.reply({ content: `You already have an active Permanent Tokens Earned Boost.`, ephemeral: true });
+          int.editReply({ content: `You already have an active Permanent Tokens Earned Boost.`, ephemeral: true });
         }
       } else if (selectedItem === "Novice Active Member Role") {
         if (int.member.roles.cache.has('1150870507445563452')) {
-          int.reply({ content: `You already have the role!`, ephemeral: true });
+          int.editReply({ content: `You already have the role!`, ephemeral: true });
           return;
         }
 
@@ -56,7 +57,7 @@ module.exports = {
           await int.member.roles.add('1150871272822161408');
         }
 
-        int.reply({ content: `You have obtained the Novice Active Member Role!`, ephemeral: true });
+        int.editReply({ content: `You have obtained the Novice Active Member Role!`, ephemeral: true });
 
 
       } else if (localConstants.channelCreationActions.includes(selectedItem)) {
@@ -79,7 +80,7 @@ module.exports = {
           ],
         })
           .then(async (channel) => {
-            int.reply({ content: `Commission channel created: ${channel}`, ephemeral: true });
+            int.editReply({ content: `Commission channel created: ${channel}`, ephemeral: true });
             const comEmbed = new EmbedBuilder()
               .setColor('#f26e6a')
               .setImage('https://puu.sh/JPffc/3c792e61c9.png')
@@ -101,12 +102,12 @@ module.exports = {
           })
           .catch((error) => {
             console.error('Error creating the commission channel:', error);
-            int.reply({ content: 'An error occurred while creating the commission channel.', ephemeral: true });
+            int.editReply({ content: 'An error occurred while creating the commission channel.', ephemeral: true });
           });
 
       } else if (selectedItem === "Advanced Active Member Role") {
         if (int.member.roles.cache.has('1150870529104949330')) {
-          int.reply({ content: `You already have the role!`, ephemeral: true });
+          int.editReply({ content: `You already have the role!`, ephemeral: true });
           return;
         }
 
@@ -117,12 +118,12 @@ module.exports = {
           await int.member.roles.add('1150871272822161408');
         }
 
-        int.reply({ content: `You have obtained the Advanced Active Member Role!`, ephemeral: true });
+        int.editReply({ content: `You have obtained the Advanced Active Member Role!`, ephemeral: true });
 
 
       } else if (selectedItem === "Ultimate Active Member Role") {
         if (int.member.roles.cache.has('1150870546842660904')) {
-          int.reply({ content: `You already have the role!`, ephemeral: true });
+          int.editReply({ content: `You already have the role!`, ephemeral: true });
           return;
         }
 
@@ -133,25 +134,25 @@ module.exports = {
           await int.member.roles.add('1150871272822161408');  
         }
 
-        int.reply({ content: `You have obtained the **Ultimate** Active Member Role!`, ephemeral: true });
+        int.editReply({ content: `You have obtained the **Ultimate** Active Member Role!`, ephemeral: true });
 
 
       } else if (selectedItem === "Mirage I Perk") {
         if (int.member.roles.cache.has('1146644779464151151')) {
-          int.reply({ content: `You already have this perk active!`, ephemeral: true });
+          int.editReply({ content: `You already have this perk active!`, ephemeral: true });
           return;
         }
 
         await int.member.roles.add('1146644779464151151');
 
 
-        int.reply({ content: `You have obtained the Mirage I Perk! Make sure to check <#871874459169071165>`, ephemeral: true });
+        int.editReply({ content: `You have obtained the Mirage I Perk! Make sure to check <#871874459169071165>`, ephemeral: true });
 
 
       } else if (selectedItem === "Global Boost") {
         const announcementsChannel = int.guild.channels.cache.get('764561474000912434');
         localFunctions.applyGlobalBoost(4, 24);
-        int.reply({ content: `Global boost applied!`, ephemeral: true });
+        int.editReply({ content: `Global boost applied!`, ephemeral: true });
         announcementsChannel.send(`<@&1107112464455311400> <@${int.user.id}> has activated a global 4X Token Boost for 24 hours!`);
 
       } else if (itemObject.isReturnable) {
@@ -159,7 +160,7 @@ module.exports = {
         let backgroundOnUse = onUseItems.find((item) => item.type === 'background');
         if (backgroundOnUse) {
           if (backgroundOnUse.name === selectedItem) {
-            int.reply({ content: `You're already using this cosmetic!`, ephemeral: true });
+            int.editReply({ content: `You're already using this cosmetic!`, ephemeral: true });
             return;
           }
           onUseItems = onUseItems.filter((item) => item.type !== 'background');
@@ -167,9 +168,9 @@ module.exports = {
         }
         onUseItems.push(itemObject);
         await localFunctions.setOnUse(userId, onUseItems, collection);
-        int.reply({ content: `Cosmetic succesfully enabled!`, ephemeral: true });
+        int.editReply({ content: `Cosmetic succesfully enabled!`, ephemeral: true });
       }else {
-        int.reply({ content: `Something went wrong...`, ephemeral: true });
+        int.editReply({ content: `Something went wrong...`, ephemeral: true });
         return;
       }
 
@@ -181,7 +182,7 @@ module.exports = {
         await localFunctions.setInventory(userId, userInventory, collection);
       }
     } else {
-      int.reply({ content: `You've already used ${selectedItem}!`, ephemeral: true });
+      int.editReply({ content: `You've already used ${selectedItem}!`, ephemeral: true });
     }
   }
 };
