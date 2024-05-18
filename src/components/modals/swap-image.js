c771ee8a8271a07401888b5a2981d049bb7b3a25
XegSo <xeresonance@gmail.com>
2024-05-17 23:09:22 -0500

    embed pimp

--- src/components/modals/swap-image.js
+++ src/components/modals/swap-image.js
@@ -32,10 +32,22 @@ module.exports = {
     let status = 'Pending';
     let collab = initializedMap.get(int.user.id).collab;
     let imageSwapEmbed = new EmbedBuilder()
+    .setFooter({ text: "Endless Mirage | Image Request", iconURL: 'https://puu.sh/JP9Iw/a365159d0e.png' })
       .setColor('#f26e6a')
       .setImage(imageURL)
       .setTimestamp()
-      .setTitle(`\`\`\`New image swap request by ${int.user.tag}\`\`\`\n${collab.name}`);
+      .setTitle(`\`\`\`🏐 New image swap request!\`\`\``)
+      .addFields(
+        {
+            name: "‎",
+            value: `┌ User: ${int.user.tag}\n└ Collab: ${collab.name}`,
+        },
+        {
+            name: "‎",
+            value: "<:01:1195440946989502614><:02:1195440949157970090><:03:1195440950311387286><:04:1195440951498391732><:05:1195440953616502814><:06:1195440954895765647><:07:1195440956057604176><:08:1195440957735325707><:09:1195440958850998302><:10:1195441088501133472><:11:1195441090677968936><:12:1195440961275306025><:13:1195441092036919296><:14:1195441092947103847><:15:1195441095811797123><:16:1195440964907573328><:17:1195441098768789586><:19:1195441100350034063><:21:1195441102585606144><:23:1195440971886903356><:25:1195441155664527410><:27:1195440974978093147>",
+        },
+
+    )
 
     const message = await requestChannel.send({
       embeds: [imageSwapEmbed],
