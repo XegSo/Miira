63460984bea8c805c3eb7e9a609c8df556c0f4d1
XegSo <xeresonance@gmail.com>
2024-06-10 15:38:37 -0500

    cache error fix (hopefully memory wont leak)

--- src/components/buttons/join-collab.js
+++ src/components/buttons/join-collab.js
@@ -62,7 +62,6 @@ module.exports = {
                 osuData: buttonCache.get(int.user.id).osuData,
                 userCollabData: buttonCache.get(int.user.id).userCollabData
             })
-            buttonCache.delete(int.user.id);
 
         } catch (e) {
             console.log(e);
